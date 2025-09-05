19e2f155065691afc6297414b48837795eeb2e42
ak <ananthakrishna.k@iiitb.ac.in>
2025-09-05 12:17:43 +0530

    combi

--- dbg.hpp
+++ dbg.hpp
@@ -4,13 +4,13 @@ using namespace std;
 #define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
 
 // void __print(int x) {cerr << x;}
-// void __print(long x) {cerr << x;}
+void __print(long x) {cerr << x;}
 void __print(long long x) {cerr << x;}
 void __print(unsigned x) {cerr << x;}
 void __print(unsigned long x) {cerr << x;}
 void __print(unsigned long long x) {cerr << x;}
-// void __print(float x) {cerr << x;}
-// void __print(double x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
 void __print(long double x) {cerr << x;}
 void __print(char x) {cerr << '\'' << x << '\'';}
 void __print(const char *x) {cerr << '\"' << x << '\"';}
@@ -23,4 +23,4 @@ template<typename T>
 void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
 void _print() {cerr << "]\n";}
 template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}                                                                                                                                                
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
--- libra/data_structures/ordered_set.cpp
+++ libra/data_structures/ordered_set.cpp
@@ -1,25 +1,12 @@
 #include<bits/stdc++.h>
 
-using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T> using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-typedef __gnu_pbds::tree<long long, __gnu_pbds::null_type, less<long long>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
-
-// ordered set/ordered_multiset :two additional operations in log(n)
-    // order_of_key: The number of items in a set that are strictly smaller than k
-    // find_by_order: It returns an iterator to the ith largest element
+//* X.find_by_order(k) return kth element. 0 indexed.
+//* X.order_of_key(k) returns count of elements strictly less than k.
 
 
-
-// int main(){
-//     int n;
-//     cin>>n;
-//     ordered_multiset st;
-//     for(int i=0;i<n;i++){
-//         int t;
-//         cin>>t;
-//         st.insert(t);
-//     }
-//     cout<<st.order_of_key(2);
-// }
